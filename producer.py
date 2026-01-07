076a1aa8214b5b8b03ed149fcaaf708ad400cedc
inam <manikrish2003@gmail.com>
2026-01-07 15:10:49 -0500

    changing from scraping HTML to rss

--- producer.py
+++ producer.py
@@ -3,6 +3,7 @@ from kafka import KafkaProducer
 import json
 import time
 from bs4 import BeautifulSoup
+import feedparser
 
 KAFKA_TOPIC="Financial_news"
 KAFKA_BROKER="localhost:9092"
@@ -12,25 +13,29 @@ producer=KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
-URL = "https://www.w3schools.com/html/html_headings.asp"
+URL = "https://feeds.marketwatch.com/marketwatch/topstories"
 
 def scrape_headlines():
-    resp=requests.get(URL)
-    soup=BeautifulSoup(resp.text,"lxml")
+    # resp=requests.get(URL)
+    feed=feedparser.parse(URL)
     headlines=[]
-    for h in soup.find_all("h2"):
-        text=h.get_text().strip()
-        if text:
-            headlines.append(text)
+    for entry in feed.entries[:5]:
+        print(entry.id)
+        headlines.append(entry.title.strip())
+        
     # print(headlines)
     return headlines
 
+
+
 while True:
+
     news=scrape_headlines()
     for headline in news:
+
         msg={"text":headline,"timestamp": time.time()}
         producer.send(KAFKA_TOPIC, msg)
         print("Sent: ",msg)
-    time.sleep(0.5)
+    time.sleep(5)
     
         
